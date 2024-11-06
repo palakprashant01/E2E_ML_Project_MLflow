042bf11b75b2ed43baebc609a6a74e900f1db8c3
Palak Prashant <palakprashant01@gmail.com>
2024-11-06 11:23:56 -0500

    folder structure included

---
+++ template.py
@@ -0,0 +1,53 @@
+#We need files and folders to manage this code. We will create a python script for this.
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level = logging.INFO, format = '[%(asctime)s]: %(message)s:') #We want to see the log in our terminal: information-level log with time stamp and execution message
+
+#project name
+
+projectName = "ml_proj"
+
+#arranging folder and file name
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{projectName}/__init__.py",
+    f"src/{projectName}/components/__init__.py",
+    f"src/{projectName}/utils/__init__.py",
+    f"src/{projectName}/utils/common.py",
+    f"src/{projectName}/config/__init__.py",
+    f"src/{projectName}/config/configuration.pcley",
+    f"src/{projectName}/pipeline/__init__.py",
+    f"src/{projectName}/entity/__init__.py",
+    f"src/{projectName}/entity/config_entity.py",
+    f"src/{projectName}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "schema.yaml",
+    "main.py",
+    "app.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+    "templates/index.html"
+
+
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath) #converting everything to Path
+    filedir, file = os.path.split(filepath)
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filepath}")
+    
+    if(not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, 'w') as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+    else:
+        logging.info(f"{file} already exists")
